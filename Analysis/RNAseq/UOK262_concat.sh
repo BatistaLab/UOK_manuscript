919d82e6786c599be748ef7569ff4c3567666f81
Christina Fitzsimmons <33811247+FitzsimmonsCM@users.noreply.github.com>
2023-03-27 15:31:40 -0400

    feat: add rnaseq concat script

---
+++ Analysis/RNAseq/UOK262_concat.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+# HC files
+# Sample 4
+cat /data/BatistaLab_NGS/UOK262_268trio/HFJMYBGXB/Sample_4_HCA/4_HCA_S4_R1_001.fastq /data/BatistaLab_NGS/UOK262_268trio/HGKCHBGXB/Sample_4_HCA/4_HCA_S4_R1_001.fastq > /data/BatistaLab_NGS/UOK262_268trio/Combined_Reads/262Reads/HC/HCA_S4_comb_R1.fastq
+cat /data/BatistaLab_NGS/UOK262_268trio/HFJMYBGXB/Sample_4_HCA/4_HCA_S4_R2_001.fastq /data/BatistaLab_NGS/UOK262_268trio/HGKCHBGXB/Sample_4_HCA/4_HCA_S4_R2_001.fastq > /data/BatistaLab_NGS/UOK262_268trio/Combined_Reads/262Reads/HC/HCA_S4_comb_R2.fastq
+
+# Sample 5
+cat /data/BatistaLab_NGS/UOK262_268trio/HFJMYBGXB/Sample_5_HCB/5_HCB_S5_R1_001.fastq /data/BatistaLab_NGS/UOK262_268trio/HGKCHBGXB/Sample_5_HCB/5_HCB_S5_R1_001.fastq > /data/BatistaLab_NGS/UOK262_268trio/Combined_Reads/262Reads/HC/HCB_S5_comb_R1.fastq
+cat /data/BatistaLab_NGS/UOK262_268trio/HFJMYBGXB/Sample_5_HCB/5_HCB_S5_R2_001.fastq /data/BatistaLab_NGS/UOK262_268trio/HGKCHBGXB/Sample_5_HCB/5_HCB_S5_R2_001.fastq > /data/BatistaLab_NGS/UOK262_268trio/Combined_Reads/262Reads/HC/HCB_S5_comb_R2.fastq
+
+# Sample 6
+cat /data/BatistaLab_NGS/UOK262_268trio/HFJMYBGXB/Sample_6_HCC/6_HCC_S6_R1_001.fastq /data/BatistaLab_NGS/UOK262_268trio/HGKCHBGXB/Sample_6_HCC/6_HCC_S6_R1_001.fastq > /data/BatistaLab_NGS/UOK262_268trio/Combined_Reads/262Reads/HC/HCC_S6_comb_R1.fastq
+cat /data/BatistaLab_NGS/UOK262_268trio/HFJMYBGXB/Sample_6_HCC/6_HCC_S6_R2_001.fastq /data/BatistaLab_NGS/UOK262_268trio/HGKCHBGXB/Sample_6_HCC/6_HCC_S6_R2_001.fastq > /data/BatistaLab_NGS/UOK262_268trio/Combined_Reads/262Reads/HC/HCC_S6_comb_R2.fastq
+
+# HW files
+# Sample 7
+cat /data/BatistaLab_NGS/UOK262_268trio/HFJMYBGXB/Sample_7_HWA/7_HWA_S7_R1_001.fastq /data/BatistaLab_NGS/UOK262_268trio/HGKCHBGXB/Sample_7_HWA/7_HWA_S7_R1_001.fastq > /data/BatistaLab_NGS/UOK262_268trio/Combined_Reads/262Reads/HW/HWA_S7_comb_R1.fastq
+cat /data/BatistaLab_NGS/UOK262_268trio/HFJMYBGXB/Sample_7_HWA/7_HWA_S7_R2_001.fastq /data/BatistaLab_NGS/UOK262_268trio/HGKCHBGXB/Sample_7_HWA/7_HWA_S7_R2_001.fastq > /data/BatistaLab_NGS/UOK262_268trio/Combined_Reads/262Reads/HW/HWA_S7_comb_R2.fastq
+
+# Sample 8
+cat /data/BatistaLab_NGS/UOK262_268trio/HFJMYBGXB/Sample_8_HWB/8_HWB_S8_R1_001.fastq /data/BatistaLab_NGS/UOK262_268trio/HGKCHBGXB/Sample_8_HWB/8_HWB_S8_R1_001.fastq > /data/BatistaLab_NGS/UOK262_268trio/Combined_Reads/262Reads/HW/HWB_S8_comb_R1.fastq
+cat /data/BatistaLab_NGS/UOK262_268trio/HFJMYBGXB/Sample_8_HWB/8_HWB_S8_R2_001.fastq /data/BatistaLab_NGS/UOK262_268trio/HGKCHBGXB/Sample_8_HWB/8_HWB_S8_R2_001.fastq > /data/BatistaLab_NGS/UOK262_268trio/Combined_Reads/262Reads/HW/HWB_S8_comb_R2.fastq
+
+# Sample 9
+cat /data/BatistaLab_NGS/UOK262_268trio/HFJMYBGXB/Sample_9_HWC/S1_9_HWC_S9_R1_001.fastq /data/BatistaLab_NGS/UOK262_268trio/HGKCHBGXB/Sample_9_HWC/9_HWC_S9_R1_001.fastq > /data/BatistaLab_NGS/UOK262_268trio/Combined_Reads/262Reads/HW/HWC_S9_comb_R1.fastq
+cat /data/BatistaLab_NGS/UOK262_268trio/HFJMYBGXB/Sample_9_HWC/S1_9_HWC_S9_R2_001.fastq /data/BatistaLab_NGS/UOK262_268trio/HGKCHBGXB/Sample_9_HWC/9_HWC_S9_R2_001.fastq > /data/BatistaLab_NGS/UOK262_268trio/Combined_Reads/262Reads/HW/HWC_S9_comb_R2.fastq
